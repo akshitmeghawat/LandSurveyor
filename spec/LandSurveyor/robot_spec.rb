97d645571e05efdf65b2a16f627b241f53c63d66
Akshit Meghawat <am1292@gmail.com>
2015-02-05 11:10:49 +0530

    Wrote specs for moving for each direction

--- spec/LandSurveyor/robot_spec.rb
+++ spec/LandSurveyor/robot_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe "Robot" do
 
 	let (:robot1) { Robot.new(1, 1, 'NORTH')}
+	let (:robot2) { Robot.new(2, 2, 'WEST')}
+	let (:robot3) { Robot.new(3, 3, 'EAST')}
+	let (:robot4) { Robot.new(4, 4, 'SOUTH')}
 
 	describe "Basic checks" do
 
@@ -30,6 +33,30 @@ describe "Robot" do
 		it "rotate right" do
 			robot1.turn_right
 			expect(robot1.check_orientation).to eq('EAST')
+		end
+
+	end
+
+	describe "Move functions" do
+
+		it "checking moving when facing north" do
+			robot1.move
+			expect(robot1.y_position).to eq(2)
+		end
+
+		it "checking moving when facing west" do
+			robot2.move
+			expect(robot2.x_position).to eq(1)
+		end
+
+		it "checking moving when facing east" do
+			robot3.move
+			expect(robot3.x_position).to eq(4)
+		end
+
+		it "checking moving when facing south" do
+			robot4.move
+			expect(robot3.x_position).to eq(3)
 		end
 
 	end
