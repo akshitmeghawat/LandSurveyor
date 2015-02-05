adc1b536af45886396f5278b3d5f5faf3e3f427f
Akshit Meghawat <am1292@gmail.com>
2015-02-05 10:56:14 +0530

    Wrote specs for rotate.

--- spec/LandSurveyor/robot_spec.rb
+++ spec/LandSurveyor/robot_spec.rb
@@ -2,18 +2,36 @@ require 'spec_helper'
 
 describe "Robot" do
 
-	let (:robot1) { Robot.new(0, 0, 'NORTH')}
+	let (:robot1) { Robot.new(1, 1, 'NORTH')}
 
-	it "checking x position of robot" do
-		expect(robot1.x_position).to eq(0)
+	describe "Basic checks" do
+
+		it "checking x position of robot" do
+			expect(robot1.x_position).to eq(1)
+		end
+
+		it "checking y position of robot" do
+			expect(robot1.y_position).to eq(1)
+		end
+
+		it "checking orientation of robot" do
+			expect(robot1.check_orientation).to eq('NORTH')
+		end
+
 	end
 
-	it "checking y position of robot" do
-		expect(robot1.y_position).to eq(0)
+	describe "Rotate Functions" do
+
+		it "rotate left" do
+			robot1.turn_left
+			expect(robot1.check_orientation).to eq('WEST')
+		end
+
+		it "rotate right" do
+			robot1.turn_right
+			expect(robot1.check_orientation).to eq('EAST')
+		end
+
 	end
 
-	it "checking orientation of robot" do
-		expect(robot1.check_orientation).to eq('NORTH')
-	end
-	
 end
